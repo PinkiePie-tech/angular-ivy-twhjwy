f1979f122eb6da17da49806bca582157e4cd4f18
PinkiePie-tech <sebastien.pujol@gmail.com>
2022-07-26 11:30:58 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-twhjwy
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-twhjwy)
---
+++ src/app/form/form.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+/**
+ * Le but est simple pour ce petit exercice, vous devez mettre en place le formulaire, ainsi que ça validation avec les messages d'erreur
+ * Attention, certain de ses blocks sont une seule est unique information, le validateur ne doit donc pas être appliqué à une seule input
+ * Pas vraiment d'indice pour faire ce boulot, de 1 j'ai la flemme d'en écrire et de 2 vous connaissez déjà tous les outils pour réussir
+ */
+
+@Component({
+  selector: 'sip-form',
+  templateUrl: 'form.component.html',
+  styleUrls: ['form.component.scss'],
+})
+export class FormComponent {}
